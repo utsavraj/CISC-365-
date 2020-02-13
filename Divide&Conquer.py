aecad264f0e31f7c4f987431d0b06a300d693da0
Utsav Raj <49531059+utsavraj@users.noreply.github.com>
2020-02-13 18:56:44 -0500

    Update Divide&Conquer.py

--- Divide&Conquer.py
+++ Divide&Conquer.py
@@ -12,12 +12,12 @@ def Binary_Search(A, to_find):
       print("Found")
 
     if (A[mid] < to_find):
-      Binary_Search(A[mid+1:len(A)], to_find)
+      Binary_Search(A[mid+1:], to_find)
 
     if (A[mid] > to_find):
       Binary_Search(A[0:mid], to_find)
 
-
+#needs Finishing
 def Merge_Sort(A):
   if not A:
     print("Empty Array provided as input")
@@ -28,19 +28,29 @@ def Merge_Sort(A):
   else:
     mid = len(A)//2
     first_half = Merge_Sort(A[0:mid])
-    second_half = Merge_Sort(A[mid+1:len(A)])
+    second_half = Merge_Sort(A[mid+1:len(A)-1])
 
 
 
 
 #Used as a pivot to sort everything arround it. 
 def Partition(A):
+  i = 0
+  pivot = A[len(A)-1]
+
+  for j in range(0, len(A)):
+    if (A[j] <= pivot):
+      A[i],A[j] = A[j],A[i]
+      i = i + 1
+
+  A[len(A)-1], A[i+1] =  A[i+1], A[len(A)-1]
+  return (i+1)
 
 
 
 
 
-
+#needs debuging
 def Quick_Sort(A):
   if not A:
     print("Empty Array provided as input")
@@ -51,7 +61,8 @@ def Quick_Sort(A):
   else:
     q = Partition(A)
     first_half = Quick_Sort(A[0:q - 1])
-    second_half = Quick_Sort(A[q + 1:len(A)])
+    second_half = Quick_Sort(A[q + 1:len(A)-1])
+    print(A)
 
 
 
@@ -63,7 +74,7 @@ def main():
 
   #For Search Algo only
   sample_1 = [1,2,3,4,5,6]
-  Binary_Search(sample_1, 3)
+  Binary_Search(sample_1, 6)
 
   #For Sort Algo only
   sample_2 = [6,5,4,3,2,1,0]
